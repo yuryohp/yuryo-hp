4ba36b4be97481f9ecac466dce740b69dda88490
yuryohp <163832381+yuryohp@users.noreply.github.com>
2024-04-09 14:41:10 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -119,15 +119,15 @@ function reveal() {
 
 ////// NAVBAR DISAPPEARING AND APPEARING //////////
 
-if (window.innerWidth < 960) {
-  var prevScrollpos = window.scrollY;
-  window.onscroll = function() {
-    var currentScrollPos = window.scrollY;
-      if (prevScrollpos > currentScrollPos) {
-        document.getElementById("navbar").style.top = "0";
-      } else {
-        document.getElementById("navbar").style.top = "-100px";
-      }
-      prevScrollpos = currentScrollPos;
-    }
-}
+// if (window.innerWidth < 960) {
+//   var prevScrollpos = window.scrollY;
+//   window.onscroll = function() {
+//     var currentScrollPos = window.scrollY;
+//       if (prevScrollpos > currentScrollPos) {
+//         document.getElementById("navbar").style.top = "0";
+//       } else {
+//         document.getElementById("navbar").style.top = "-100px";
+//       }
+//       prevScrollpos = currentScrollPos;
+//     }
+// }
