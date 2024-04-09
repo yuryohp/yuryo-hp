67344acb38b2fc2fcf02b97c5d79b1aa118b1809
yuryohp <163832381+yuryohp@users.noreply.github.com>
2024-04-09 17:16:13 +0900

    reveal nashi

--- script.js
+++ script.js
@@ -42,20 +42,20 @@ document.querySelectorAll(".nav-link").forEach((n) =>
 
 //ページのスクロールに合わせて画像が表示される
 
-window.addEventListener("scroll", reveal);
+// window.addEventListener("scroll", reveal);
 
-function reveal() {
-  var reveals = document.querySelectorAll(".reveal");
-  var i;
+// function reveal() {
+//   var reveals = document.querySelectorAll(".reveal");
+//   var i;
 
-  for (i = 0; i < reveals.length; i++) {
-    if (reveals[i].getBoundingClientRect().top < window.innerHeight) {
-      reveals[i].classList.add("active");
-    } else {
-      reveals[i].classList.remove("active");
-    }
-  }
-}
+//   for (i = 0; i < reveals.length; i++) {
+//     if (reveals[i].getBoundingClientRect().top < window.innerHeight) {
+//       reveals[i].classList.add("active");
+//     } else {
+//       reveals[i].classList.remove("active");
+//     }
+//   }
+// }
 
 ////////////////////////////////////////////
 
